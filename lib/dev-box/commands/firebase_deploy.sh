214977d7eb3b3f229ed807b256a1298b5319f289
Leo Lee <leoleelf@gmail.com>
2023-04-24 10:01:38 +0800

    chore: Update firebase_deploy function with new options and features

--- lib/dev-box/commands/firebase_deploy.sh
+++ lib/dev-box/commands/firebase_deploy.sh
@@ -3,19 +3,48 @@
 GCP_SOURCE_PATH="~/source/github"
 
 function firebase_deploy {
-  local firebaseProjectName=$1
-  local repositoryPath=$2
-  local nodeVersion=$3
-  local toolVersion=$4
-  repositoryPathArray=(${repositoryPath//\// })
-  local repositoryUserName=${repositoryPathArray[0]}
-  local repositoryProjectName=${repositoryPathArray[1]}
-  echo ${firebaseProjectName}
+  while getopts "g:r:n:t:f:s:u" opt; do
+    case ${opt} in
+      g )
+        local gcpProjectName="${OPTARG}"
+        ;;
+      r )
+        local repositoryPath="${OPTARG}"
+        repositoryPathArray=(${repositoryPath//\// })
+        local repositoryUserName=${repositoryPathArray[0]}
+        local repositoryProjectName=${repositoryPathArray[1]}
+        ;;
+      n )
+        local nodeVersion="${OPTARG}"
+        ;;
+      t )
+        local toolVersion="${OPTARG}"
+        ;;
+      f )
+        local functions="${OPTARG}"
+        ;;
+      s )
+        GCP_SOURCE_PATH="${OPTARG}"
+        ;;
+      u )
+        local isUpdateSourceMode=true
+        ;;
+      \? ) # Handle invalid options
+        echo "Invalid option: -$OPTARG" >&2
+        ;;
+      : ) # Handle missing arguments
+        echo "Option -$OPTARG requires an argument" >&2
+        ;;
+    esac
+  done
+  shift $((OPTIND -1))
+  echo ${gcpProjectName}
   echo ${repositoryPath}
   echo ${repositoryUserName}
   echo ${repositoryProjectName}
   echo ${nodeVersion}
   echo ${toolVersion}
+  echo ${isUpdateSourceMode}
   source ~/.bashrc
   node -v
   echo "End."
